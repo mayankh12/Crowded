e9d5f52e190a80ec3ae6ba9ae63edeb79abd72d5
Mayank Hirani <mayankh12@vt.edu>
2021-01-31 03:29:25 -0500

    Initial commit

---
+++ livepopulartimes/__init__.py
@@ -0,0 +1,16 @@
+
+from .crowd import get_populartimes_by_formatted_address
+
+import logging
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+
+def get_populartimes_by_address(formatted_address, proxy = False):
+
+
+    return get_populartimes_by_formatted_address(formatted_address, proxy)
+
+
+
+
+
